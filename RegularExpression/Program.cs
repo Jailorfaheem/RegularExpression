e07585fe4f079c874747324b9935e46ebbecc348
faheemjailor <faheemjailor0055>
2021-11-21 19:46:15 +0530

    faheem UC2 for validation of last name

--- RegularExpression/Program.cs
+++ RegularExpression/Program.cs
@@ -8,10 +8,15 @@ namespace RegularExpression00
         {
             Console.WriteLine("Welcome to Regular Expression Program");
             Console.WriteLine();
-            //taking user input of first name
+            //taking user input and passing to FirstNameValid method
             Console.Write("Enter First Name : ");
             string firstname = Console.ReadLine();
-            RegularExpression.FirstName(firstname);
+            RegularExpression.FirstNameValid(firstname);
+            //taking user input and passing to LastNameValid method
+            Console.Write("\nEnter Last Name : ");
+            string lastName = Console.ReadLine();
+            RegularExpression.LastNameNameValid(lastName);
+
             Console.ReadLine();
 
         }
