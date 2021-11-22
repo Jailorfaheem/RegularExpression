0dde8f14969ee158efaa340f52f08e72d155539f
faheemjailor <faheemjailor0055>
2021-11-22 16:17:08 +0530

    faheem UC11 for custom Exception

--- RegularExpression/Regex.cs
+++ RegularExpression/Regex.cs
@@ -2,67 +2,148 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
-namespace RegularExpression00
+namespace UserRegistrationException
 {
-    public class RegularExpression
+    public class UserException
     {
-        //This method for matching pattern of first name
-        public static void FirstNameValid(string name)
-        {
-            //Regular expression for first letter is capital and maximum charactor is 3
-            string firstname = "^[A-Z]{1}[a-zA-Z]{2}$";
-            Regex regex = new Regex(firstname);
-            //if condition for matching pattern
-            //if condition is true then if block will execute
-            if (regex.IsMatch(name))
-            {
-                Console.WriteLine(name + " is valid name");
-            }
-            //else condition for if condition is false then else block will execute
-            else
-            {
-                Console.WriteLine(name + " is invalid first name. Please start with capital letter and only take maximum 3 charactors");
-            }
-        }
-        //This method for validate last name
-        public static void LastNameNameValid(string name)
-        {
-            //Regular expression for first letter is capital and maximum charactor is 3
-            string firstname = "^[A-Z]{1}[a-zA-Z]{2}$";
-            Regex regex = new Regex(firstname);
-            //if condition for matching pattern
-            if (regex.IsMatch(name))
-            {
-                Console.WriteLine(name + " is valid name");
-            }
-            //else condition for if condition is false then else block will execute
-            else
-            {
-                Console.WriteLine(name + " is invalid last name. Please start with capital letter and take maximums 3 charactors");
-            }
-        }
-        //This method for validation of email id
-        public static void EmailValid()
-        {
-            Console.Write("Email id : ");
-            string name = Console.ReadLine();
+        //declaring instance variable
+        public string message;
 
+        public UserException(string message)
+        {
+            this.message = message;
+        }
+        /// <summary>
+        /// Valids the first name.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="UserRegistrationException.CustomException">invalid</exception>
+        public string ValidFirstName()
+        {
+            string firstname = "^[A-Z]{1}[a-zA-Z]{2}$";
+            Regex regex = new Regex(firstname);
+            try
+            {
+                //if condition for matching pattern
+                //if condition is true then if block will execute
+                if (regex.IsMatch(message))
+                {
+                    return "valid";
+                }
+                //else condition for if condition is false then else block will execute
+                else
+                {
+                    return "invalid";
+                }
+            }
+            catch (ArgumentNullException ex)
+            {
+                throw new CustomException(CustomException.ExceptionType.Argument_Null_Exception, "invalid");
+            }
+
+        }
+        public string ValidLastName()
+        {
+            string lastname = "^[A-Z]{1}[a-zA-Z]{2}$";
+            Regex regex = new Regex(lastname);
+            try
+            {
+                //if condition for matching pattern
+                //if condition is true then if block will execute
+                if (regex.IsMatch(message))
+                {
+                    return "valid";
+                }
+                //else condition for if condition is false then else block will execute
+                else
+                {
+                    return "invalid";
+                }
+            }
+            catch (ArgumentNullException ex)
+            {
+                throw new CustomException(CustomException.ExceptionType.Argument_Null_Exception, "invalid");
+            }
+
+        }
+        public string EmailIdValid()
+        {
             string emailid = "^[a-z]{3}[.][a-z]*[@]{1}[bl]{2}[.]{1}[co]{2}[.]{1}[a-z]*$";
             Regex regex = new Regex(emailid);
-
-            if (regex.IsMatch(name))
+            try
             {
-                Console.WriteLine("Email Id is valid");
+                //if condition for matching pattern
+                //if condition is true then if block will execute
+                if (regex.IsMatch(message))
+                {
+                    return "valid";
+                }
+                //else condition for if condition is false then else block will execute
+                else
+                {
+                    return "invalid";
+                }
             }
-            else
+            catch (ArgumentNullException ex)
             {
-                Console.WriteLine("Email Is Invalid,Please Enter Valid email Id");
+                throw new CustomException(CustomException.ExceptionType.Argument_Null_Exception, "invalid");
             }
-
+        }
+        public string MobileNumValid()
+        {
+            //Regular expression of mobile number
+            string number = "^[0-9]{1,2}[ ]{1}[0-9]{10}$";
+            Regex regex = new Regex(number);
+            try
+            {
+                //if condition for matching pattern
+                //if condition is true then if block will execute
+                if (regex.IsMatch(message))
+                {
+                    return "valid";
+                }
+                //else condition for if condition is false then else block will execute
+                else
+                {
+                    return "invalid";
+                }
+            }
+            catch (ArgumentNullException ex)
+            {
+                throw new CustomException(CustomException.ExceptionType.Argument_Null_Exception, "invalid");
+            }
+        }
+        public string ValidPassword()
+        {
+            //Regular expression of mobile number
+            //Regular expression of password
+            //Rule 1 : minimum 8 charactors
+            //Rule 2 : atleast 1 uppercase
+            //Rule 3 : atleast 1 numeric number
+            //Rule 4 : ataleast 1 special charactor
+            string password = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%&*])[a-zA-Z0-9!@#$%&*]{8,}$";
+            Regex regex = new Regex(password);
+            try
+            {
+                //if condition for matching pattern
+                //if condition is true then if block will execute
+                if (regex.IsMatch(message))
+                {
+                    return "valid";
+                }
+                //else condition for if condition is false then else block will execute
+                else
+                {
+                    return "invalid";
+                }
+            }
+            catch (ArgumentNullException ex)
+            {
+                throw new CustomException(CustomException.ExceptionType.Argument_Null_Exception, "invalid");
+            }
         }
     }
-
 }
